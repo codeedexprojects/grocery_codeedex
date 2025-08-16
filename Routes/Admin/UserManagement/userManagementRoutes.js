361f4ad151311469fa7fee387488ed2f40322a01
Sharookh-Codeedex <sharookh.codeedex@gmail.com>
2025-08-16 10:39:24 +0530

    Added search to user management

--- Routes/Admin/UserManagement/userManagementRoutes.js
+++ Routes/Admin/UserManagement/userManagementRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../../Controller/Admin/UserManagement/userManagementController');
 
+
 router.get('/', userController.getAllUsers);
+router.get('/search', userController.searchUsers)
 router.get('/:id', userController.getUserById);
 router.patch('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
